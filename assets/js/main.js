18c77101697868929ce576e85382a7725dda30c5
Lucas Campos <31932085+lucasocrack@users.noreply.github.com>
2023-01-19 02:40:07 -0300

    home

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,25 +14,20 @@ $(function() {
 
 
 		setTimeout(function() {
-			
 			var counter = 0;
-      $('.site-mobile-menu .has-children').each(function(){
+      		$('.site-mobile-menu .has-children').each(function(){
         var $this = $(this);
-        
+
         $this.prepend('<span class="arrow-collapse collapsed">');
-
         $this.find('.arrow-collapse').attr({
-          'data-toggle' : 'collapse',
-          'data-target' : '#collapseItem' + counter,
+          'data-bs-toggle' : 'collapse',
+          'data-bs-target' : '#collapseItem' + counter,
         });
-
         $this.find('> ul').attr({
           'class' : 'collapse',
           'id' : 'collapseItem' + counter,
         });
-
         counter++;
-
       });
 
     }, 1000);
